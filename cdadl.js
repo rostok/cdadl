06f7943f3c8075aba16547b538802edf81dffe25
rostok <rostok@3e.pl>
2021-09-06 23:18:06 +0200

    2021 cleanup

--- cdadl.js
+++ cdadl.js
@@ -1,99 +1,19 @@
+const https = require('https')
 const spawn = require('child_process').spawn;
 var moment = require('moment');
 var fs = require('fs');
-var url = require('url');
 var util = require('util');
 var Crawler = require("crawler");
 var request = require('request').defaults({ jar: true });
 var progress = require('request-progress');
-var setCookie = require('set-cookie-parser');
 var readlineSync = require('readline-sync');
-var cookies;
-var js_beautify = require("js-beautify").js_beautify;
 
 var downloads = {};
 var filenamePrefix = "";
 var title;
 var savedResult = "";
 
-function findBetween(strToParse, strStart, strFinish) {
-    var str = strToParse.match(strStart + "(.*?)" + strFinish);
-    if (str === null) return "";
-    if (str.length < 2) return "";
-    return str[1];
-}
-
-function rot13(str) {
-    var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-    var index = x => input.indexOf(x);
-    var translate = x => index(x) > -1 ? output[index(x)] : x;
-    return str.split('').map(translate).join('');
-}
-
-ma = function (a) {
-    a = a.replace(".cda.mp4", "");
-    a = a.replace(".2cda.pl", ".cda.pl");
-    a = a.replace(".3cda.pl", ".cda.pl");
-    return "https://" + a + ".mp4"
-}
-ia = function (a) {
-    for (var b = [], e = 0; e < a.length; e++) {
-        var f = a.charCodeAt(e);
-        b[e] = 33 <= f && 126 >= f ? String.fromCharCode(33 + (f + 14) % 94) : String.fromCharCode(f)
-    }
-    return ma(b.join(""))
-}
-ja = function (a) {
-    String.fromCharCode(("Z" >= a ? 82 : 132) >= (c = a.charCodeAt(0) + 11) ? c : c - 55);
-    return ia(a)
-}
-la = function (a) {
-    return decodeURIComponent(a)
-}
-ka = function (a) {
-    return ja(la(L(a)))
-}
-L = function (a) {
-    return a.replace(/[a-zA-Z]/g, function (a) {
-        return String.fromCharCode(("Z" >= a ? 90 : 122) >= (a = a.charCodeAt(0) + 13) ? a : a - 26)
-    })
-}
-M = function (a) {
-    String.fromCharCode(("Z" >= a ? 11 : 344) >= (c = a.charCodeAt(0) + 22) ? c : c - 11);
-    a = a.replace("_XDDD", "");
-    a = a.replace("_CDA", "");
-    a = a.replace("_ADC", "");
-    a = a.replace("_CXD", "");
-    a = a.replace("_QWE", "");
-    a = a.replace("_Q5", "");
-    a = a.replace("_IKSDE", "");
-    a = ka(L(a));
-    return a;
-}
-
 function downloadMovie(movieURL) {
-    // in 2020 urls use M()
-    if (M(movieURL) == M("")) {
-        throw "empty movieURL";
-    }
-    if (M(movieURL).includes(".mp4")) {
-        console.log("input:\t", movieURL);
-        movieURL = M(movieURL);
-        console.log("M():\t", movieURL);
-        // url has extra 3 characters before extension just to get HTTP 302 
-        //movieURL = movieURL.replace(".mp4","").slice(0,-3)+".mp4";
-        //console.log("trim3:", movieURL);
-    }
-    // in 2019 urls are ROT13 encoded
-    if (movieURL.startsWith("uggc")) {
-        console.log("input:\t", movieURL);
-        movieURL = rot13(movieURL);
-        console.log("rot13:\t", movieURL);
-        // url has extra 3 characters before extension just to get HTTP 302 
-        movieURL = movieURL.replace(".mp4", "").slice(0, -3) + ".mp4";
-        console.log("trim3:\t", movieURL);
-    }
     if (movieURL === null || movieURL === "" || movieURL.indexOf(".mp4") === -1) {
         console.log("aborting, bad videourl:", movieURL);
         fs.writeFileSync('result.html', savedResult, {
@@ -102,6 +22,7 @@ function downloadMovie(movieURL) {
         return;
     }
     console.log("v.url:\t", movieURL);
+
     var startTime = process.hrtime();
     var fl = movieURL.split("?")[0].split("/").pop().trim();
 
@@ -112,7 +33,7 @@ function downloadMovie(movieURL) {
     console.log("outfile:", fl);
 
     console.log("aria2c ", movieURL, " -o ", fl);
-    //console.log(process.argv.join("|"));
+
     if (process.argv[process.argv.length - 1].includes("aria")) {
         console.log("calling aria...");
         spawn('aria2c', [movieURL, "-o", fl], {
@@ -130,21 +51,17 @@ function downloadMovie(movieURL) {
     } catch (e) {}
 
     var headers = {
-        'User-Agent': 'cdadl',
+        'User-Agent': 'CDADL',
         'Cookie': '',
         'Accept-Ranges': 'bytes',
     };
-
-    cookies.forEach(function (e) {
-        headers.Cookie += e.name + "=" + e.value + "; ";
-    });
 
     request({
         url: movieURL,
         method: "HEAD",
         headers: headers
     }, function callback(err, inc, res) {
-        //	try {    	
+        //    try {        
         if (inc == undefined) {
             console.log("inc:", inc);
             console.log("err:", err);
@@ -232,8 +149,8 @@ function downloadMovie(movieURL) {
         }
         //    }
         //    catch (ex) {
-        //    	console.log("downloadUrl failed, exiting");
-        //       	process.exit();
+        //        console.log("downloadUrl failed, exiting");
+        //           process.exit();
         //    }
     });
 }
@@ -248,109 +165,58 @@ var c = new Crawler({
 
         savedResult = result.body;
 
-        cookies = setCookie.parse(result);
         try {
-            if (result.uri.indexOf("?wers") == -1) {
-                // first get the formats
-
-                var formatArr = [];
-                $('a.quality-btn').each(function () {
-                    formatArr.push($(this).html());
-                });
-                if (formatArr.length === 0) formatArr.push("unknown");
-                console.log("formats:  " + formatArr.join(" "));
-
-                // if format is passed into command line
-                if (process.argv.length > 3 && formatArr.indexOf(process.argv[3]) > -1) {
-                    var fmt = process.argv[3];
-                    if ($("a.quality-btn:contains(" + fmt + ")").length) {
-                        $("a.quality-btn:contains(" + fmt + ")").last().each(function (a) {
-                            c.queue(this.attribs.href);
-                            return;
-                        });
-                    }
-                } else {
-                    // if not get the best
-                    if ($("a.quality-btn").length) {
-                        $("a.quality-btn").last().each(function (a) {
-                            //console.log(this.attribs.href);
-                            c.queue(this.attribs.href);
-                            return;
-                        });
-                    } else {
-                        // get anything
-                        c.queue(result.uri + "?wers");
-                    }
-                }
-            } else {
-                //console.log(result.body);
-                // then get the video
-                title = $("title").text();
-                console.log("title:    " + title);
-                console.log("fetching: " + result.uri);
-                //$("[id^=mediaplayer]").each((a,e)=>{ console.log("e:"+e.attribs.id); });
-                $("[id^=mediaplayer]").first().each(function (a) {
-                    var movieURL = "";
-                    try {
-                        var id = this.attribs.id;
-                        var ss = "document.getElementById\\(\\'" + this.attribs.id + "\\'\\).href = \\'";
-                        movieURL = findBetween(result.body, ss, "\\';");
-                        downloadMovie(movieURL);
-                    } catch (ex) {
-                        console.log("first method failed:" + ex);
-                    }
-
-                    try {
-                        if (movieURL === null || movieURL === "") {
-                            movieURL = JSON.parse(this.attribs.player_data).video.file;
-                            //console.log(this.attribs.player_data);
-                            //	 	fs.writeFileSync('result.html', result.body, {flags:'w+'});
-                            //        process.exit();
-                            //						movieURL = movieURL.slice(0,-2);
-                            downloadMovie(movieURL);
-                        }
-                    } catch (ex) {
-                        console.log("second method failed:" + ex);
-                        console.log(ex.stack);
-                    }
-                });
-
-                // try {
-                //     // old method didn't work so try with new one. its obfuscated JS so use http://deobfuscatejavascript.com/# 
-                //     var output="";
-                //     DEOBEVILJS = eval;
-                //     eval = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     window = {};
-                //     document = {};
-                //     window.eval = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     write = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     document.write = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     writeln = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     document.writeln = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     createPopup = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     window.createPopup = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     createElement = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     document.createElement = function(input_string){var out = js_beautify(input_string); output = out;};
-                //     appendChild = function(input_string){var out = js_beautify(input_string); output = out;};
-
-                //     var script = $("script").text();
-                //     if (script!==null && script.indexOf("eval(function(p,")>-1) {
-                //         var code = "eval(function(p,a,c,k,e,d)" + script.split("eval(function(p,a,c,k,e,d)")[1].split("\n")[0];
-                //         DEOBEVILJS(code); // 'output' holds the result
-                //         url = output.split('<video src="')[1].split('" style="')[0];
-                //         downloadMovie(url);
-                //     }
-                // } catch (ex) {
-                //     console.log("third method failed");
-                //     console.log(ex);
-                // }
-                
+            // first get the formats
+            var playerDataJSON = $("div[player_data]").attr("player_data") || "";
+            if (playerDataJSON=="") {
+                throw "player_data JSON not found";
             }
-        } catch (ex) {
+            var playerData = JSON.parse(playerDataJSON);
+            if (!playerData || !playerData.video || !playerData.video.qualities) throw "player_data has no qualities";
+            var qs = playerData.video.qualities;
+            var formats = Object.keys(qs).concat(Object.values(qs));
+            console.log("formats:  "+formats.join(" "));
+
+            if (formats.length==0) throw "no formats found";
+
+            var fmt = formats[formats.length-1];
+            // if format is passed into command line
+            if (process.argv.length > 3 && formats.indexOf(process.argv[3]) > -1) fmt = process.argv[3];
+            fmt = Object.values(qs).find(v => v == fmt || v == qs[fmt]) ;
+
+            title = $("title").text();
+            console.log("title:    " + title);
+            console.log("fetching: " + result.uri);
+
+            // get the video link with jsonrpc request via https
+            console.log("req...");
+            const data = `{"jsonrpc":"2.0","method":"videoGetLink","params":["${playerData.video.id}","${fmt}",${playerData.video.ts},"${playerData.video.hash2}",{}],"id":1}`;
+
+            https.request(
+            				"https://www.cda.pl/",
+            				{method:'POST',headers:{'User-Agent':'Chrome'}},
+            				res => {
+                            		res.on('data', d => { 
+                            			// videoGetLink response received
+                            			d = d.toString();
+                            			var r = JSON.parse(d);
+                            			if (r.result.status!="ok") {
+                            				console.error("-------------------");
+                            				console.error(res.req._header+data);
+                            				console.error("-------------------");
+                            				console.error(d);
+                            				console.error("-------------------");
+                            				throw "videoGetLink status not ok";
+                            			}
+                            			downloadMovie(r.result.resp); 
+                            		})
+                			})
+                .on('error', error => { throw "videoGetLink error "+error;})
+                .end(data);
+        } 
+        catch (ex) {
             console.error("unexpected error happened, see result.html and investigate the website");
-            fs.writeFileSync('result.html', result.body, {
-                flags: 'w+'
-            });
+            fs.writeFileSync('result.html', result.body, { flags: 'w+' });
             console.log(ex);
             process.exit();
         }
